c62e4c290eb283f963b35fadd0c2d56dcc00aaca
Mathias Lindblom <mathias.lindblom@trustly.com>
2015-03-27 12:20:48 +0100

    Correct row distances. Splitted PHP files

---
+++ index.php
@@ -0,0 +1,100 @@
+<?php
+require("head.php");
+
+// This is the "Title"-page or the first information below the header
+require("jumbotron_header.php");
+?>
+  <h1>Online läxhjälp</h1>
+  <p>Välkommen till online läxhjälp. Tanken med hemsidan är att hjälpa elever som studerar årskurs X - X med sina studier.</p>
+        <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Mer info »</a></p> -->
+<?php require("jumbotron_footer.php");?>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <div class="ih-item square effect13 bottom_to_top">
+            <a href="#">
+              <div class="img">
+                <img src="resources/images/mathematics.jpg" alt="...">
+              </div>
+              <div class="info">
+                <h3>Matematik</h3>
+                <p>Description goes here</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="ih-item square effect13 bottom_to_top">
+            <a href="#">
+              <div class="img">
+                <img src="resources/images/physics.jpg" alt="...">
+              </div>
+              <div class="info">
+                <h3>Fysik</h3>
+                <p>Description goes here</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="ih-item square effect13 bottom_to_top">
+            <a href="#">
+              <div class="img">
+                <img src="resources/images/chemistry.jpg" alt="...">
+              </div>
+              <div class="info">
+                <h3>Kemi</h3>
+                <p>Description goes here</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-4">
+          <div class="ih-item square effect13 bottom_to_top">
+            <a href="#">
+              <div class="img">
+                <img src="resources/images/chemistry.jpg" alt="...">
+              </div>
+              <div class="info">
+                <h3>Kemi</h3>
+                <p>Description goes here</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="ih-item square effect13 bottom_to_top">
+            <a href="#">
+              <div class="img">
+                <img src="resources/images/chemistry.jpg" alt="...">
+              </div>
+              <div class="info">
+                <h3>Kemi</h3>
+                <p>Description goes here</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="ih-item square effect13 bottom_to_top">
+            <a href="#">
+              <div class="img">
+                <img src="resources/images/chemistry.jpg" alt="...">
+              </div>
+              <div class="info">
+                <h3>Kemi</h3>
+                <p>Description goes here</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+
+<?php
+require("footer.php");
+?>
